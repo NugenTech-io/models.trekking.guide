89aaf1003a62b4d1e4fda045fbb8d619dfc336c0
rajivstha <stha.rajiv@gmail.com>
2021-07-19 19:20:05 +0545

    gude signup model added

---
+++ GuideSignup.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const GuideSignupSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    required: true,
+    max: 32
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: true,
+    max: 32
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true
+  },
+  mobileNumber: {
+    type: Number,
+    required: true,
+    unique: true
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.models.GuideSignup || mongoose.model('GuideSignup', GuideSignupSchema)
